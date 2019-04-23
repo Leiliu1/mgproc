b22ec813f8b25456003b54abe4af12229c329cc0
Lei Liu <lei.liu.1@stonybrook.edu>
2019-04-23 15:18:04 -0400

    delete HNPS stuff

---
+++ usercode/load_metrics.py
@@ -0,0 +1,28 @@
+# we define our own function,
+# which can then be used for defining metrics
+#
+# see memory_measure in tree_values.py for
+# an example of a realistic metric function
+
+
+# load metrics
+base = metrics_from_file(inputfile = './metrics/base', ranks = 1)
+filter_u = metrics_from_file(inputfile = './metrics/filtered_u', ranks = 1)
+
+# load names as short cuts
+nomv_rtmv = 'nomv_rtmv'
+nomv_ppmv = 'nomv_ppmv'
+nomv_rmnt = 'nomv_rmnt'
+
+
+def compare_heavy_np(name, metrics):
+	# name = nomv_rtmv, nomv_ppmv, nomv_rmnt
+	# metrics = base, filter_u
+
+	filename = f"./comparisons/heavy_np/{name}"
+	result = comparisons_from_file(inputfile = filename, directory = './trees/', metrics = metrics)
+
+	result.show()
+
+	return result
+	
